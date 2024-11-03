848ce402db7c19231ce6ae4704c80708b6db6e5a
daniloaraujosp <danilo.araujo.sp@gmail.com>
2024-11-03 07:23:39 -0300

    Resolvendo desafio

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,18 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    public class Iphone : Smartphone
     {
+
+        public Iphone(string numero, string modelo, string imei, int memoria) : base(numero)
+        {
+        }
+
         // TODO: Sobrescrever o método "InstalarAplicativo"
+
+        internal override void InstalarAplicativo()
+        {
+            Console.WriteLine($"Instalando o aplicativo Instagram da Play Store.");
+        }
     }
 }
--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -1,9 +1,15 @@
+
 namespace DesafioPOO.Models
 {
     public abstract class Smartphone
     {
+
         public string Numero { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        private string Modelo { get; set; }
+        private string Imei { get; set; }
+        private int Memoria { get; set; }
+        
 
         public Smartphone(string numero)
         {
@@ -21,6 +27,6 @@ namespace DesafioPOO.Models
             Console.WriteLine("Recebendo ligação...");
         }
 
-        public abstract void InstalarAplicativo(string nomeApp);
+        internal abstract void InstalarAplicativo();
     }
 }
